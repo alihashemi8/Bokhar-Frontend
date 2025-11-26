6a26a91b36d498d848c58efabf811997e5e1e8b5
ali <alihashemi5553@gmail.com>
2025-11-26 19:27:25 +0330

    style : darkmode and shadow for KPIs

--- src/components/admin/reports/KPICard.jsx
+++ src/components/admin/reports/KPICard.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 export default function KPICard({ title, value }) {
 return (
-<div className="bg-white p-4 rounded-lg shadow-sm flex flex-col">
-<div className="text-xs text-slate-500">{title}</div>
-<div className="mt-3 text-xl font-semibold text-slate-800">{value ?? '—'}</div>
+<div className="bg-white dark:bg-gray-800  p-4 rounded-lg shadow-sm hover:dark:shadow-gray-400  flex flex-col">
+<div className="text-xs text-slate-500 dark:text-white ">{title}</div>
+<div className="mt-3 text-xl font-semibold text-slate-800 dark:text-white ">{value ?? '—'}</div>
 </div>
 );
 }
