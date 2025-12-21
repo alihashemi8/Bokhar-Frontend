bac6cc93e5adb35e791e1984769747e7daa0629d
ali <alihashemi5553@gmail.com>
2025-12-21 13:18:30 +0330

    style:redesign the color of  StepProgress

--- src/components/orders/StepProgress.jsx
+++ src/components/orders/StepProgress.jsx
@@ -16,7 +16,9 @@ export default function StepProgress({ steps, step, maxStep, onStepClick }) {
           <div
             key={item.id}
             className={`flex flex-col items-center w-full transition-all ${
-              isClickable ? "cursor-pointer hover:opacity-90" : "cursor-not-allowed opacity-60"
+              isClickable
+                ? "cursor-pointer hover:opacity-90"
+                : "cursor-not-allowed opacity-60"
             }`}
             onClick={() => isClickable && onStepClick(item.id)}
           >
@@ -25,10 +27,10 @@ export default function StepProgress({ steps, step, maxStep, onStepClick }) {
               className={`w-10 h-10 flex items-center justify-center rounded-full border-2 transition-all duration-300
                 ${
                   isActive
-                    ? "bg-pink-500 text-white border-pink-400 shadow-md"
+                    ? "bg-sky-300 text-white border-sky-300 shadow-md"
                     : isCompleted
-                    ? "bg-pink-200 text-pink-700 border-pink-300"
-                    : "bg-white border-pink-100 text-gray-400"
+                    ? "bg-sky-100 text-sky-700 border-sky-200"
+                    : "bg-white border-sky-100 text-gray-400"
                 }`}
             >
               {isCompleted && !isActive ? "✓" : item.id}
@@ -38,9 +40,9 @@ export default function StepProgress({ steps, step, maxStep, onStepClick }) {
             <span
               className={`mt-2 text-sm font-medium ${
                 isActive
-                  ? "text-pink-600"
+                  ? "text-sky-600"
                   : isCompleted
-                  ? "text-pink-500"
+                  ? "text-sky-500"
                   : "text-gray-400"
               }`}
             >
